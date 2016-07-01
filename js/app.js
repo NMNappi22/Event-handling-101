eec24eb16692ec5bba812d4a6dc3c86d645db1d7
NMNappi22 <NMNappi22@gmail.com>
2016-06-30 15:01:43 -1000

    W3D4

---
+++ js/app.js
@@ -0,0 +1,14 @@
+window.onload = initiateProgram
+
+function intiateProgram (){
+	//var for click one
+	var clickerLink = document.getElementById('clicker');
+	clickerLink.addEventListener('click', handleClick);
+
+	function handleClick(evt){
+  		innerHTML
+	}
+
+
+
+}
