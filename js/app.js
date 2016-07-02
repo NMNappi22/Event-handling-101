976edce428a82cda2bfbb45208a1b5e3883399ec
NMNappi22 <NMNappi22@gmail.com>
2016-07-01 15:02:57 -1000

    W3D5

--- js/app.js
+++ js/app.js
@@ -1,14 +1,39 @@
-window.onload = initiateProgram
+window.onload = initiateProgram;
 
-function intiateProgram (){
-	//var for click one
+function initiateProgram(){
+	//first button
+	var clickLinkOne = document.getElementById('otherClicker');
+	clickLinkOne.addEventListener('click', handleClicker);
+	
+	function handleClicker(evt){
+		
+	}
+
+	//second button
 	var clickerLink = document.getElementById('clicker');
 	clickerLink.addEventListener('click', handleClick);
 
 	function handleClick(evt){
-  		innerHTML
+  		console.log(evt.target.id);
 	}
 
+	//hoverVanTransform
+	var hoverVan = document.getElementById('otherClicker');
+	hoverVan.addEventListener('mouseover', handleHover);
+	hoverVan.addEventListener('mouseleave', releaseHover);
 
+	var hoverTwo = document.getElementById('clicker');
+	hoverTwo.addEventListener('mouseover', handleHover);
+	hoverTwo.addEventListener('mouseleave', releaseHover);
+
+	function handleHover(evt){
+		var parent = evt.target.parentElement;
+		parent.className = 'hoverStyles';
+	}
+
+	function releaseHover(evt){
+		var parent = evt.target.parentElement;
+		parent.className = 'button-1'; 
+	}
 
 }
