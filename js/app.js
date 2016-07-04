fc2ec52f8a8429ef4b1e6b9c029dccd7812d2532
Noah Nappi <NoahNappi@Mins-MacBook.local>
2016-07-03 16:05:02 -1000

    I FINISHED

--- js/app.js
+++ js/app.js
@@ -4,17 +4,18 @@ function initiateProgram(){
 	//first button
 	var clickLinkOne = document.getElementById('otherClicker');
 	clickLinkOne.addEventListener('click', handleClicker);
-	
+	var inc = 0
 	function handleClicker(evt){
-		
+  		inc = inc + 1;
+    	document.getElementById("click").innerHTML = ("You clicked me " + inc + " times");  
 	}
 
 	//second button
-	var clickerLink = document.getElementById('clicker');
+	var clickerLink = document.getElementById('You');
 	clickerLink.addEventListener('click', handleClick);
 
 	function handleClick(evt){
-  		console.log(evt.target.id + " clicked me, Yay!");
+  		document.getElementById("clicky").innerHTML = (evt.target.id + " Clicked Me");
 	}
 
 	//hoverVanTransform
@@ -22,9 +23,14 @@ function initiateProgram(){
 	hoverVan.addEventListener('mouseover', handleHover);
 	hoverVan.addEventListener('mouseleave', releaseHover);
 
-	var hoverTwo = document.getElementById('clicker');
+	var hoverTwo = document.getElementById('You');
 	hoverTwo.addEventListener('mouseover', handleHover);
 	hoverTwo.addEventListener('mouseleave', releaseHover);
+
+	var hoverThree = document.getElementById('buttonTwo');
+	hoverThree.addEventListener('mouseover', handleHover);
+	hoverThree.addEventListener('mouseleave', releaseHover);
+
 
 	function handleHover(evt){
 		var parent = evt.target.parentElement;
@@ -35,5 +41,7 @@ function initiateProgram(){
 		var parent = evt.target.parentElement;
 		parent.className = 'button-1'; 
 	}
+
+
 
 }
